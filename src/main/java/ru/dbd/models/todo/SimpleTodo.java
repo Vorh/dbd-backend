756731592909c7e00e462597778b7463e9c05ecf
vorh <sorcerer12@list.ru>
2017-04-03 22:21:26 +0300

    ADD [ model todo ]

---
+++ src/main/java/ru/dbd/models/todo/SimpleTodo.java
@@ -0,0 +1,12 @@
+package ru.dbd.models.todo;
+
+/**
+ * Created by vorh on 4/3/17.
+ */
+public class SimpleTodo implements Todo{
+
+
+    public TypeTodo getType() {
+        return null;
+    }
+}
---
+++ src/main/java/ru/dbd/models/todo/Todo.java
@@ -0,0 +1,9 @@
+package ru.dbd.models.todo;
+
+/**
+ * Created by vorh on 4/3/17.
+ */
+public interface Todo {
+
+    TypeTodo getType();
+}
