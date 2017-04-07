eb7eb24efbcd951073cb1c4cb84c1bac84f5bc53
vorh <sorcerer12@list.ru>
2017-04-07 22:08:57 +0300

    ADD [ body for getUserByLogin ]

--- src/main/java/ru/dbd/dao/user/UserDaoBasic.java
+++ src/main/java/ru/dbd/dao/user/UserDaoBasic.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Repository;
 import ru.dbd.dao.AbstractDao;
 import ru.dbd.models.user.User;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 /**
  * Created by vorh on 4/6/17.
  */
 
 @Repository
+@Transactional
 public class UserDaoBasic extends AbstractDao implements UserDao{
 
 
@@ -20,11 +24,17 @@ public class UserDaoBasic extends AbstractDao implements UserDao{
 
     public void saveUser(User user) {
         getSession().persist(user);
+
     }
 
     public User getUserByLogin(String login) {
-        Query query = getSession().createQuery("FROM user WHERE login = :login ");
+        Query query = getSession().createQuery("FROM User WHERE login = :login ");
         query.setParameter("login",login);
-        return (User) query.list().get(0);
+        List list = query.list();
+        if (list.size() ==0){
+            return null;
+        }else {
+            return (User) query.list().get(0);
+        }
     }
 }
