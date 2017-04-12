6634edf90b65510df2fd550db7127b58b734103c
vorh <sorcerer12@list.ru>
2017-04-12 22:47:13 +0300

    EDIT [ show_sql false , add small TodoController ]

--- src/main/java/ru/dbd/controllers/todo/TodoController.java
+++ src/main/java/ru/dbd/controllers/todo/TodoController.java
@@ -1,19 +1,20 @@
 package ru.dbd.controllers.todo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.dbd.models.todo.Todo;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by vorh on 4/4/17.
  */
 
-@RestController
+@Controller
 public class TodoController {
 
 
-    @RequestMapping
-    public Todo getSimpleTodo(){
-        return  new Todo();
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String getTodoList(){
+        System.out.println("TEST");
+        return  null;
     }
 }
