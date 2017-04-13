fc8326718b2fccdbdc9bff5b52f81f039bf1443c
vorh <sorcerer12@list.ru>
2017-04-13 23:11:31 +0300

    ADD [ getTodo , getTodoList Controller ]

--- src/main/java/ru/dbd/controllers/todo/TodoController.java
+++ src/main/java/ru/dbd/controllers/todo/TodoController.java
@@ -1,8 +1,15 @@
 package ru.dbd.controllers.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.dbd.models.todo.Todo;
+import ru.dbd.services.todo.TodoService;
+
+import java.util.List;
 
 /**
  * Created by vorh on 4/4/17.
@@ -11,10 +18,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TodoController {
 
+    @Autowired
+    private TodoService todoService;
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String getTodoList(){
-        System.out.println("TEST");
-        return  null;
+
+    @RequestMapping(value = "/todoList",method = RequestMethod.GET)
+    public @ResponseBody List<Todo> getTodoList(){
+        return todoService.getTodoList();
+    }
+
+    @RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
+    public @ResponseBody Todo getTodo(@PathVariable("id") int id){
+        return todoService.getTodoById(id);
     }
 }
