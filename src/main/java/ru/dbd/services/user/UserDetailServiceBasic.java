9984455de133baf56c28f9963b61a4651f51f662
vorh <sorcerer12@list.ru>
2017-04-15 22:48:38 +0300

    ADD [ custom userDetails : UserPrincipal ]

--- src/main/java/ru/dbd/services/user/UserDetailServiceBasic.java
+++ src/main/java/ru/dbd/services/user/UserDetailServiceBasic.java
@@ -1,17 +1,12 @@
 package ru.dbd.services.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.dbd.dao.user.UserDao;
 import ru.dbd.models.user.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by vorh on 4/14/17.
@@ -29,14 +24,10 @@ public class UserDetailServiceBasic implements UserDetailsService{
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userDao.getUserByLogin(s);
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return createUser(user,authorities);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        return userPrincipal;
     }
 
-    private org.springframework.security.core.userdetails.User createUser(User user, List<GrantedAuthority> list){
-        return new org.springframework.security.core.userdetails.User(user.getLogin(),user.getPassword(),list);
-    }
 
 
     public void setUserDao(UserDao userDao) {
---
+++ src/main/java/ru/dbd/services/user/UserPrincipal.java
@@ -0,0 +1,64 @@
+package ru.dbd.services.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.dbd.models.user.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by vorh on 4/15/17.
+ */
+public class UserPrincipal implements UserDetails{
+
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<>();
+        list.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return list;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+}
