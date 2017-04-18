578fe491042928a35fec269a0ac5976fd6647cb9
vorh <sorcerer12@list.ru>
2017-04-18 23:12:55 +0300

    ADD [ LoginController validate ]

--- src/main/java/ru/dbd/controllers/LoginController.java
+++ src/main/java/ru/dbd/controllers/LoginController.java
@@ -1,9 +1,14 @@
 package ru.dbd.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.dbd.models.login.StatusLogin;
+import ru.dbd.models.user.User;
+import ru.dbd.services.login.LoginService;
 
 /**
  * Created by vorh on 4/17/17.
@@ -13,17 +18,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
 
-    @RequestMapping(value = "/verification",method = RequestMethod.GET)
-    public String login(@RequestParam("username") String username ,
-                        @RequestParam("password") String password){
+    @Autowired
+    private LoginService loginService;
 
-        System.out.println(username);
-        System.out.println(password);
-        return "TEST";
+    @RequestMapping(value = "/verification",method = RequestMethod.GET)
+    public @ResponseBody StatusLogin login(@RequestParam("username") String username ,
+          @RequestParam("password") String password){
+        User user = new User();
+        user.setPassword(password);
+        user.setLogin(username);
+        StatusLogin validate = loginService.validate(user);
+        System.out.println(validate.name());
+        return validate;
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String loginForm(){
         return "sign.html";
+    }
+
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
     }
 }
