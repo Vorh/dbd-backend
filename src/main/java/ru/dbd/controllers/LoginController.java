f0b665c654f40f3c0937abff4f2e6d0b8be93dc8
vorh <sorcerer12@list.ru>
2017-04-19 22:02:42 +0300

    [ EDIT ] /login replace /auth , add token in cookie response

--- src/main/java/ru/dbd/controllers/LoginController.java
+++ src/main/java/ru/dbd/controllers/LoginController.java
@@ -1,14 +1,19 @@
 package ru.dbd.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import ru.dbd.models.login.StatusLogin;
 import ru.dbd.models.user.User;
 import ru.dbd.services.login.LoginService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by vorh on 4/17/17.
@@ -21,23 +26,35 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @RequestMapping(value = "/verification",method = RequestMethod.GET)
-    public @ResponseBody StatusLogin login(@RequestParam("username") String username ,
-          @RequestParam("password") String password){
+    @Autowired
+    private CsrfTokenRepository tokenRepository;
+
+    @RequestMapping(value = "/verification", method = RequestMethod.GET)
+    public void login(@RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      HttpServletResponse response) {
         User user = new User();
         user.setPassword(password);
         user.setLogin(username);
         StatusLogin validate = loginService.validate(user);
-        System.out.println(validate.name());
-        return validate;
+        response.addHeader("TESt","TEST");
+
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String loginForm(){
+    @RequestMapping(value = "/auth", method = RequestMethod.GET)
+    public String loginForm(HttpServletRequest request,HttpServletResponse response) {
+        CsrfToken csrfToken = tokenRepository.generateToken(request);
+        Cookie cookie = new Cookie(csrfToken.getHeaderName(),csrfToken.getToken());
+        response.addHeader(csrfToken.getHeaderName(),csrfToken.getToken());
+        response.addCookie(cookie);
         return "sign.html";
     }
 
     public void setLoginService(LoginService loginService) {
         this.loginService = loginService;
+    }
+
+    public void setTokenRepository(CsrfTokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
     }
 }
