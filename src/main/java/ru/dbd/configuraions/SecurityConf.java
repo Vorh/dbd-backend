34b84b8aaab2cdbd953259a010335f8a48ce8972
vorh <sorcerer12@list.ru>
2017-04-19 22:01:15 +0300

    [ ADD ] bean csrf token repository

--- src/main/java/ru/dbd/configuraions/SecurityConf.java
+++ src/main/java/ru/dbd/configuraions/SecurityConf.java
@@ -1,12 +1,15 @@
 package ru.dbd.configuraions;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 /**
  * Created by vorh on 4/14/17.
@@ -33,6 +36,13 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll();
 
+    }
+
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
     }
 
     @Autowired
