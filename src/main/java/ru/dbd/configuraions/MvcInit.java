c306919d58e4b778576ae11a5f2d8122b93a9370
vorh <sorcerer12@list.ru>
2017-04-21 20:57:24 +0300

    [ ADD ] switch profile depending on the parameter PROD

--- src/main/java/ru/dbd/configuraions/MvcInit.java
+++ src/main/java/ru/dbd/configuraions/MvcInit.java
@@ -2,11 +2,24 @@ package ru.dbd.configuraions;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 /**
  * Created by vorh on 4/12/17.
  */
 public class MvcInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        String prod = System.getProperty("prod");
+        if (prod ==null){
+            servletContext.setInitParameter("spring.profiles.active","dev");
+        }else {
+            servletContext.setInitParameter("spring.profiles.active","prod");
+        }
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
